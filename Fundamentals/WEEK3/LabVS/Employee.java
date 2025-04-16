174ed8bf0741a9e63679246af55d461db4224e8f
Mr Lii <leetamparega@gmail.com>
2025-04-16 23:13:35 +0000

    lab starter

--- Fundamentals/WEEK3/LabVS/Employee.java
+++ Fundamentals/WEEK3/LabVS/Employee.java
@@ -1,5 +1,6 @@
+import java.util.Objects;
 
-public class Employee<T> {
+public class Employee<T> implements Comparable<Employee<T>> {
 
     private T employeeId;
     private String name;
@@ -27,4 +28,83 @@ public class Employee<T> {
         this.employeeId = employeeId;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getPerformanceRating() {
+        return performanceRating;
+    }
+
+    public void setPerformanceRating(double performanceRating) {
+        this.performanceRating = performanceRating;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                ", performanceRating=" + performanceRating +
+                ", yearsOfExperience=" + yearsOfExperience +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee<T> other) {
+        // Sort by years of experience in descending order
+        return Integer.compare(other.yearsOfExperience, this.yearsOfExperience);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee<?> employee = (Employee<?>) o;
+        return Objects.equals(employeeId, employee.employeeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employeeId);
+    }
 }
